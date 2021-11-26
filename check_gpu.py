2a76737af18ffdf4a11479574c8d1d1613c3267d
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2021-11-26 12:46:29 +0900

    Log all GPUs

--- check_gpu.py
+++ check_gpu.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import torch
 import cv2
+import torch
 
 print(f"{torch.cuda.is_available()=}")
 print(f"{cv2.cuda.getCudaEnabledDeviceCount()=}")
 if torch.cuda.is_available():
-    print(torch.cuda.get_device_name())
-    print(torch.cuda.get_device_capability())
-
+    for i in range(cv2.cuda.getCudaEnabledDeviceCount()):
+        print(f"Device {i}", '"' + torch.cuda.get_device_name(i) + '"', torch.cuda.get_device_capability(i))
