ad59cdb98bf32b22febe2073034d8bca030d4884
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2021-12-07 12:14:49 +0900

    Update entry.sh
    Close #7

--- entry.sh
+++ entry.sh
@@ -4,13 +4,12 @@ set -e
 USER_ID=${LOCAL_UID:-1000}
 GROUP_ID=${LOCAL_GID:-1000}
 
-# Start sshd
+# Start sshd as root
 /usr/sbin/sshd
 
 # Add the dev user
-adduser --quiet --gecos "" --disabled-password --uid ${USER_ID} --home /home/dev dev > /dev/null 2>&1
+groupadd --gid ${GROUP_ID} dev
+useradd --home-dir /home/dev --uid ${USER_ID} --gid ${GROUP_ID} --shell /bin/bash dev
 sed -i -e "s/dev:x:/dev::/g" /etc/passwd
-# chown -R dev:dev /home/dev
 
 exec gosu dev "$@"
-
