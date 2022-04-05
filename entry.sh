f52798472cba5b956d08c1fdf10d8e1b3ec54463
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2022-04-06 00:37:56 +0900

    Check the existance of user before executing useradd.
    This is useful when you run jupyter with docker compose.

--- entry.sh
+++ entry.sh
@@ -8,8 +8,13 @@ GROUP_ID=${LOCAL_GID:-1000}
 /usr/sbin/sshd
 
 # Add the dev user
-groupadd --gid ${GROUP_ID} dev
-useradd --home-dir /home/dev --uid ${USER_ID} --gid ${GROUP_ID} --shell /bin/bash dev
-sed -i -e "s/dev:x:/dev::/g" /etc/passwd
+if ! id dev &>/dev/null ; then
+  echo "Creating dev user with UID=${USER_ID}, GID=${GROUP_ID}"
+  groupadd --gid ${GROUP_ID} dev
+  useradd --home-dir /home/dev --uid ${USER_ID} --gid ${GROUP_ID} --shell /bin/bash dev
+  sed -i -e "s/dev:x:/dev::/g" /etc/passwd
+else
+  echo "User dev already exists."
+fi
 
 exec gosu dev "$@"
