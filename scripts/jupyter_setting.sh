067191bb4ab98457fbde1e32ea0e9f65e7c3904a
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2022-07-20 14:11:13 +0900

    Give fixed token

--- scripts/jupyter_setting.sh
+++ scripts/jupyter_setting.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 set -eux
 
+JUPYTER_TOKEN=password_koi-jyputer
+
 conda activate
 jupyter lab --generate-config
 jupyter_lab_config="$(jupyter --config-dir)/jupyter_lab_config.py"
@@ -16,6 +18,7 @@ sed -i \
     -e "s:# c.\(.*\).root_dir = '':c.\1.root_dir = '$NOTEBOOK_DIR':" \
     -e "s/# c.\(.*\).allow_hidden = False/c.\1.allow_hidden = True/" \
     -e "s/# c.\(.*\).open_browser = True/c.\1.open_browser = False/" \
+    -e "s/# c.ServerApp.token = '<generated>'/c.ServerApp.token = '$JUPYTER_TOKEN'"
     ${jupyter_lab_config}
 
 mkdir -p "$(jupyter --config-dir)/lab/user-settings/@jupyterlab"
