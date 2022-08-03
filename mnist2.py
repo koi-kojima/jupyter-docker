585c5d56ad0a3acf012684034557bfc529df9bad
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2022-08-03 11:07:22 +0900

    Improved lightning module.

--- mnist2.py
+++ mnist2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 
+import mnist_dataset
 import pytorch_lightning as pl
 import torch.optim
 import torchmetrics
@@ -9,13 +11,11 @@ from torch import nn
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
-import mnist_dataset
-
 
 class MNISTModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        activation = nn.GELU  # Change here to `nn.ReLU` to use ReLU.
+        activation = nn.PReLU  # Change here to `nn.ReLU` to use ReLU.
         self.conv1 = nn.Sequential(
             nn.Conv2d(1, 32, (3, 3)),  # 28x28x1 -> 26x26x32
             activation(),
@@ -54,24 +54,38 @@ class MNISTModel(pl.LightningModule):
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3)
         return optimizer
 
-    def training_step(self, batch, batch_index) -> STEP_OUTPUT:
+    def _do_prediction(self, batch):
         x, y = batch
-        y_pred = self.soft_max(self.fc2(self.feature(x)))
-        loss = self.loss_function(y_pred, y)
-        self.accuracy(y_pred, y)
+        y_prediction = self.soft_max(self.forward(x))
+        loss = self.loss_function(y_prediction, y)
+        self.accuracy(y_prediction, y)
+        return loss
+
+    def training_step(self, batch, batch_index) -> STEP_OUTPUT:
+        loss = self._do_prediction(batch)
         self.log("train_accuracy", self.accuracy)
         self.log("train_loss", loss)
         return loss
 
+    def validation_step(self, batch, batch_index) -> Optional[STEP_OUTPUT]:
+        loss = self._do_prediction(batch)
+        self.log("val_accuracy", self.accuracy)
+        self.log("val_loss", loss)
+        return loss
+
     def test_step(self, batch, batch_index) -> Optional[STEP_OUTPUT]:
-        return self.training_step(batch, batch_index)
+        loss = self._do_prediction(batch)
+        self.log("test_accuracy", self.accuracy)
+        self.log("test_loss", loss)
+        return loss
 
 
 class MNISTLoader(pl.LightningDataModule):
     _train_data: Dataset
+    _validation_data: Dataset
     _test_data: Dataset
 
     def __init__(self, mini_batch: int, dataset_name: str):
@@ -82,24 +96,40 @@ class MNISTLoader(pl.LightningDataModule):
             transforms.ToTensor(),
             transforms.Normalize((0.5,), (0.5,)),
         ])
+        self.validation_ratio = 0.2
 
     def prepare_data(self, stage: Optional[str] = None) -> None:
         dataset_class = mnist_dataset.get_mnist_dataset(self.dataset_name)
-        self._train_data = dataset_class(root="./data", train=True, download=True, transform=self.transform)
+        train_all = dataset_class(root="./data", train=True, download=True, transform=self.transform)
+        train_size = len(train_all)
+        self._train_data, self._validation_data = torch.utils.data.random_split(train_all, [
+            int(train_size * (1 - self.validation_ratio)), int(train_size * self.validation_ratio)])
         self._test_data = dataset_class(root="./data", train=False, download=True, transform=self.transform)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self._train_data, batch_size=self.mini_batch, num_workers=4)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self._validation_data, batch_size=self.mini_batch, num_workers=4)
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(self._test_data, batch_size=self.mini_batch, num_workers=4)
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Run MNIST.")
+    parser.add_argument("-b", "--batch", type=int, help="batch size (default=512)", default=512)
+    parser.add_argument("-e", "--epoch", type=int, help="epoch count", default=5)
+    parser.add_argument("-d", "--dataset", type=str, help="dataset name (default=MNIST)", default="MNIST")
+    parser.add_argument("--device", type=str, help="device", choices=["gpu", "cpu"], default="gpu")
+    args = parser.parse_args()
+    print(f"Batch Size={args.batch}, Dataset={args.dataset}, Epoch={args.epoch}")
+
     model = MNISTModel()
-    loader = MNISTLoader(512, "KMNIST")
-    trainer = Trainer(max_epochs=5, gpus=1)
+    loader = MNISTLoader(args.batch, args.dataset)
+    trainer = Trainer(max_epochs=args.epoch, gpus=1 if args.device == "gpu" else 0)
     trainer.fit(model, loader)
+    trainer.validate(model, loader)
     trainer.test(model, loader)
 
 
