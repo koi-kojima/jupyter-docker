013d5402c1f966c0daa5652920dcc475b2a834e6
koi-kojima <85006786+koi-kojima@users.noreply.github.com>
2022-10-23 23:23:53 +0900

    Show device info if cv2 is not available.

--- check_gpu.py
+++ check_gpu.py
@@ -5,5 +5,6 @@ import torch
 print(f"{torch.cuda.is_available()=}")
 print(f"{cv2.cuda.getCudaEnabledDeviceCount()=}")
 if torch.cuda.is_available():
-    for i in range(cv2.cuda.getCudaEnabledDeviceCount()):
-        print(f"Device {i}", '"' + torch.cuda.get_device_name(i) + '"', torch.cuda.get_device_capability(i))
+    print(f"Current device id: {torch.cuda.current_device()}")
+    for i in range(torch.cuda.device_count()):
+        print(f"Device {i} '{torch.cuda.get_device_name(i)}' {torch.cuda.get_device_capability(i)}")
